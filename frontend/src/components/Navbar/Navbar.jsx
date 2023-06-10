c4d1f936328ad2801b4af2ebd6ff8e018ebdaa1c
Chayannn <chayanp188@gmail.com>
2023-06-10 23:15:41 +0530

    adding navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import './Navbar.scss'
+import React from "react";
+import "./Navbar.scss";
+import { images } from "../../constants/index.js";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="app__navbar">
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "work", "skills", "contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
